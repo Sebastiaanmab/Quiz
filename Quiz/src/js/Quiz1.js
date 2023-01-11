68b3a726034cbd629bee4878973d1d7c4f64674b
Sebastiaanmab <122456926+Sebastiaanmab@users.noreply.github.com>
2023-01-11 16:05:23 +0100

    Update Quiz1.js

--- Quiz/src/js/Quiz1.js
+++ Quiz/src/js/Quiz1.js
@@ -1,1 +1,89 @@
-index.js
+function check(){
+
+   
+
+            
+
+            let Goed = 0;
+
+            result = document.getElementById('result');
+
+
+
+                if (document.getElementById('symptom').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom1').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom2').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom3').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom4').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom5').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom6').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom7').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom8').checked) {
+
+                    Goed++;
+
+                }
+
+                if (document.getElementById('symptom9').checked) {
+
+                    Goed++;
+
+                }
+
+                let numSymptoms = Goed * 10;
+
+
+
+                if (Goed >= 1000) {
+
+                    result.innerHTML = 'Alles Goed';
+
+                } else {
+
+                    result.innerHTML = 'Resultaat: ' + numSymptoms + '%';
+
+                }
+
+                return false;
+
+}
