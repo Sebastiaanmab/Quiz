dc45f6cab533765487b6c0a838d4527c6036a78b
Sebastiaanmab <122456926+Sebastiaanmab@users.noreply.github.com>
2023-01-25 16:43:16 +0100

    Update Quiz3.js

--- Quiz/src/js/Quiz3.js
+++ Quiz/src/js/Quiz3.js
@@ -1,1 +1,21 @@
-index.js
+let goed = 0;
+let count = 1;
+
+function check() {
+
+while (count < 11){
+    let vraag ='vraag' + count;
+      count++;
+
+    if (document.getElementById(vraag).checked) {goed++;}    
+
+    console.log(goed)
+    console.log(vraag)
+}
+
+let naam = document.getElementById('text').value;
+let cijfer = goed * 10;
+
+result.innerHTML = naam + ' je resultaat = ' + cijfer + '%';
+
+}
